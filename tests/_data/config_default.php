65e4d9d98f832c81da6b7aed618409b8534acd0b
Felix Pfister <mail@felix-pfister.de>
2018-09-06 18:35:11 +0200

    add test directory

---
+++ tests/_data/config_default.php
@@ -0,0 +1,14 @@
+<?php
+
+use Spryker\Shared\Kernel\KernelConstants;
+
+$config[KernelConstants::PROJECT_NAMESPACES] = [
+    'Pyz',
+];
+
+$config[KernelConstants::CORE_NAMESPACES] = [
+    'FondOfSpryker',
+    'SprykerShop',
+    'SprykerEco',
+    'Spryker',
+];
